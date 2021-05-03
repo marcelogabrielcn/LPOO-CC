eaf45938e580832f3e266cbfe723b7590e4a5ec3
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-05-03 20:25:02 -0300

    Segundo Commit

--- teste00.java
+++ teste00.java
@@ -1,20 +1,52 @@
 package AtividadeSemestral01;
 
-public class teste00 {
+import java.util.Scanner;
+
+public class teste01 {
     public static void main(String[] args) {
-        String palavraInicial = "Hello Word!";
 
-        char[] letras = palavraInicial.toCharArray();
+        Scanner teclado = new Scanner(System.in);
 
-        //for (int i = 0; i < letras.length; i++) {
-        //    System.out.println(letras[i]);
-        //}
+        System.out.println("Informe o CPF: ");
+        String cpfteste = teclado.nextLine();
+        char[] cpf = cpfteste.toCharArray();
 
-        for (char letra : letras) {
-           System.out.println(letra);
+        // Verificar 1o digito
+        int mult = 10;
+        int soma = 0;
+        int d1;
+        for (char num : cpf){
+            if (mult >= 2) {
+                System.out.println(num);
+                int val = Integer.parseInt(String.valueOf(num));
+                int res = val * mult;
+                System.out.println(res);
+                soma = soma + res;
+                mult--;
+            }
         }
+        d1 = 11 - (soma % 11);
 
-        System.out.println("quantidade de letras: " + letras.length);
+        // Verificar 2o digito
+        int mult2 = 11;
+        int soma2 = 0;
+        int d2;
+        for (char num : cpf){
+            if (mult2 >= 2){
+                System.out.println(num);
+                int val2 = Integer.parseInt(String.valueOf(num));
+                int res2 = val2 * mult2;
+                System.out.println(res2);
+                soma2 += res2;
+                mult2--;
+            }
+        }
+        d2 = 11 - (soma2 % 11);
 
+        System.out.println(cpfteste);
+        System.out.println(soma);
+        System.out.println(d1);
+        System.out.println(soma2);
+        System.out.println(d2);
     }
 }
