4f7a676f729333c70a37268c377ac5527c7487bf
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-05-03 20:53:23 -0300

    Add files via upload

--- AtividadeSemestral/PessoaFisica.java
+++ AtividadeSemestral/PessoaFisica.java
@@ -1,29 +1,5 @@
-package AtividadeSemestral;
+package AtividadeSemestral01;
 
 public class PessoaFisica {
-    private String cpf = "Ainda não Informado";
-
-    private boolean cpfValido(String txtCpf) {
-        boolean status = false;
-        // ###----------------------------------------###
-        // sua lógica de validação do CPF deve estar aqui
-
-        // ###----------------------------------------###
-        return status;
-    }
-
-    // Getters e Setters
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    // Ao inserir dado no CPF, validar antes de preencher
-    public void setCpf(String cpf) {
-        if (this.cpfValido(cpf))
-            this.cpf = cpf;
-        else
-            System.out.println("[Problemas com CPF informado!]");
-    }
 
 }
