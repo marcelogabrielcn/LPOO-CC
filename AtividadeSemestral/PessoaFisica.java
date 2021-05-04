4b2c6e224a7f4a9cf060bba1caea9a6df52e24f3
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-05-03 21:12:34 -0300

    Terceiro Commit

--- AtividadeSemestral/PessoaFisica.java
+++ AtividadeSemestral/PessoaFisica.java
@@ -1,5 +1,75 @@
 package AtividadeSemestral01;
 
 public class PessoaFisica {
+    private String cpf = "Ainda não informado!";
+    private String nome = "";
 
+    // Métodos personalizados
+    public boolean cpfValido(String txtCpf){
+        boolean status = false;
+        char[] cpf = txtCpf.toCharArray();
+
+        // Verificar 1o digito
+        int mult = 10;
+        int soma = 0;
+        int d1;
+        for (char num : cpf) {
+            if (mult >= 2) {
+                int val = Integer.parseInt(String.valueOf(num));
+                int res = val * mult;
+                soma = soma + res;
+                mult--;
+            }
+        }
+        d1 = 11 - (soma % 11);
+        if (d1 > 9) {
+            d1 = 0;
+        }
+
+        // Verificar 2o digito
+        int mult2 = 11;
+        int soma2 = 0;
+        int d2;
+        for (char num : cpf) {
+            if (mult2 >= 2) {
+                int val2 = Integer.parseInt(String.valueOf(num));
+                int res2 = val2 * mult2;
+                soma2 += res2;
+                mult2--;
+            }
+        }
+        d2 = 11 - (soma2 % 11);
+        if (d2 > 9) {
+            d2 = 0;
+        }
+
+        // Verificando d1 e d2
+        if (String.valueOf(d1).equals(String.valueOf(cpf[9])) && (String.valueOf(d2).equals(String.valueOf(cpf[10])))) {
+            status = true;
+        }
+        return status;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    // Antes de inserir um cpf, verificar se é válido.
+    public void setCpf(String cpf) {
+        if (this.cpfValido(cpf)){
+            this.cpf = cpf;
+            System.out.println("CPF Válido!");
+        } else{
+            System.out.println("[Problemas com CPF informado!]");
+        }
+    }
 }
--- AtividadeSemestral/TestePessoa.java
+++ AtividadeSemestral/TestePessoa.java
@@ -1,7 +1,22 @@
 package AtividadeSemestral01;
 
+import java.util.Scanner;
+
 public class TestePessoa {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Informe o CPF: (sem pontos nem traço.)");
+        String txtCpf = teclado.nextLine();
 
+        if (!(txtCpf==null)){
+            PessoaFisica p1 = new PessoaFisica();
+            p1.setNome("Marcelo Gabriel");
+            p1.setCpf(txtCpf);
+
+            String txtFinal = "";
+            txtFinal = "Nome: " + p1.getNome() + " CPF: " + p1.getCpf();
+            System.out.println(txtFinal);
+        }
+        System.out.println("FIM DO TESTE");
     }
 }
